3ab938e935a7b1724903f4f92e12bfffaaf36204
designermoth <160888063+designermoth@users.noreply.github.com>
2026-01-29 11:32:21 +0000

    Testing dialogic

--- scenes/player.gd
+++ scenes/player.gd
@@ -35,7 +35,8 @@ func _physics_process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and is_character_in_range:
-		print("Hi")
+		Dialogic.VAR.relationship = 1
+		Dialogic.start("Estella")
 
 
 #region Interaction
